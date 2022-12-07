600dbe064d04786a63891c5bb406ff3a19dc19cc
MausmiS <113102214+MausmiS@users.noreply.github.com>
2022-12-07 23:28:07 +0530

    Add files via upload

---
+++ Mausmi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# returns the square of any given number
+def squareofnumber(num):
+    return (num**2)
+
+
+# In[ ]:
+
+
+# returns the cube of any given number
+def cubeofnumber(num):
+    return num*num*num
+
