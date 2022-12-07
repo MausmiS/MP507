d72e86616dcfc28e779a902e1f8d3828cd96c806
MausmiS <113102214+MausmiS@users.noreply.github.com>
2022-12-07 22:28:37 +0530

    Lab assignment3

---
+++ Square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# returns the square of any given number
+def squareofnumber(num):
+    return (num**2)
+
---
+++ SquareRoot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+# returns the square root of any given number
+def find_sqrt(num):
+    return np.sqrt(num)
+
